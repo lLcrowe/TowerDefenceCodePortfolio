f57852a9092309fb0ba90c1b7feefab45410a1e0
lLcrowe <leedh7421@naver.com>
2022-09-28 14:53:40 +0900

    누락사항
    누락으로 인한 에러 고침

--- LocalizeSystem/LocalizeDBObjectScript.cs
+++ LocalizeSystem/LocalizeDBObjectScript.cs
@@ -4,8 +4,24 @@ using lLCroweTool.Dictionary;
 namespace lLCroweTool
 {
     [CreateAssetMenu(fileName = "New LocalizeDBData", menuName = "lLcroweTool/LocalizeDBData")]
-    public class LocalizeDBObjectScript : DBObjectScriptBase
+    public class LocalizeDBObjectScript : ScriptableObject
     {
+        //데이터베이스
+
+        /// <summary>
+        /// DB 버전
+        /// </summary>
+        public string version;//적어두는법은 릴리즈노트처럼
+
+        /// <summary>
+        /// DB데이터를 적어둔 오너들
+        /// </summary>
+        public string author;
+
+        /// <summary>
+        /// DB 설명
+        /// </summary>
+        public string description;
         //로컬라이징데이터들이 들어가 있는 데이터
         [SerializeField]public LocalizeDataBible localizeDataBible = new LocalizeDataBible();//번역정보가 들어가있는 데이터시트
     }
